172a091cd35a91eb16398688ac15bee0d7f3d669
Krassypnov <krassypnov75@gmail.com>
2022-04-18 16:34:12 +0700

    Add product api

--- CatalogService/Controllers/ProductController.cs
+++ CatalogService/Controllers/ProductController.cs
@@ -25,10 +25,80 @@ namespace CatalogService.Controllers
             
             if (obj == null)
                 return NotFound();
+            return Json(obj);
+        }
 
-            
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        [HttpGet("getById/{id}")]
+        public ActionResult getProductById(Guid id)
+        {
+            var obj = _db.Products.FirstOrDefault(c => c.Id == id);
+            if (obj == null)
+                return NotFound();
 
             return Json(obj);
+        }
+
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        [HttpGet("getByName/{name}")]
+        public ActionResult getProductById(string name)
+        {
+            var obj = _db.Products.FirstOrDefault(c => c.ProductName == name);
+            if (obj == null)
+                return NotFound();
+
+            return Json(obj);
+        }
+
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest)]
+        [HttpPost("addProduct/{name}, {category}, {brand}")]
+        public ActionResult addProduct(string name, string category, string brand)
+        {
+            var product = _db.Products.FirstOrDefault(c => c.ProductName == name);
+            if (product != null)
+                return BadRequest("Product already exists");
+
+            var objCategory = _db.Categories.FirstOrDefault(c => c.CategoryName == category);
+            if (objCategory == null)
+                return BadRequest("This product category does not exist");
+            var objBrand = _db.Brands.FirstOrDefault(c => c.BrandName == brand);
+            if (objBrand == null)
+                return BadRequest("This brand does not exist");
+            _db.Products.Add(new Product { ProductName = name, Category = category, Brand = brand });
+            _db.SaveChanges();
+
+            return Ok("Product added successfully");
+        }
+
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        [HttpDelete("deleteProductByName/{name}")]
+        public ActionResult deleteProductByName(string name)
+        {
+            var obj = _db.Products.FirstOrDefault(c => c.ProductName == name);
+            if (obj == null)
+                return NotFound();
+
+            _db.Remove(obj);
+            _db.SaveChanges();
+            return Ok("Product successfully deleted");
+        }
+
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        [HttpDelete("deleteProductById/{id}")]
+        public ActionResult deleteProductByName(Guid id)
+        {
+            var obj = _db.Products.FirstOrDefault(c => c.Id == id);
+            if (obj == null)
+                return NotFound();
+
+            _db.Remove(obj);
+            _db.SaveChanges();
+            return Ok("Product successfully deleted");
         }
     }
 }
