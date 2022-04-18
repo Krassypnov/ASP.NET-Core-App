6a6b10bfcb86a80a39ea984caf50fe297ca9051a
Krassypnov <krassypnov75@gmail.com>
2022-04-18 14:50:53 +0700

    Add brands api

--- CatalogService/Controllers/BrandController.cs
+++ CatalogService/Controllers/BrandController.cs
@@ -5,51 +5,49 @@ using System.Net;
 
 namespace CatalogService.Controllers
 {
-
     [Route("api/[controller]")]
     [ApiController]
-    public class CategoryController : Controller
+    public class BrandController : Controller
     {
         private readonly AppDbContext _db;
 
-        public CategoryController(AppDbContext db)
+        public BrandController(AppDbContext db)
         {
             _db = db;
         }
-        
+
         [HttpGet]
-        public ActionResult getCategories()
+        public ActionResult getBrands()
         {
-            IEnumerable<Category> obj = _db.Categories;
+            IEnumerable<Brand> obj = _db.Brands;
             return Json(obj);
         }
 
         [HttpGet("{id}")]
-        public ActionResult getCategory(Guid id)
+        public ActionResult getBrand(Guid id)
         {
-            var obj = _db.Categories.FirstOrDefault(c => c.Id == id);
+            var obj = _db.Brands.FirstOrDefault(c => c.Id == id);
             if (obj == null)
                 return Json(null);
             return Json(obj);
         }
 
-        
+
         [SwaggerResponse((int)HttpStatusCode.OK)]
         [SwaggerResponse((int)HttpStatusCode.BadRequest)]
         [HttpPost("{name}")]
-        public ActionResult addCategory(string name)
+        public ActionResult addBrand(string name)
         {
-            var obj = _db.Categories.FirstOrDefault(c => c.CategoryName == name);
+            var obj = _db.Brands.FirstOrDefault(c => c.BrandName == name);
             if (obj != null)
             {
-                return BadRequest("Category already exists");
+                return BadRequest("Brand already exists");
             }
 
-            _db.Categories.Add(new Category { CategoryName = name });
+            _db.Brands.Add(new Brand { BrandName = name });
             _db.SaveChanges();
 
             return StatusCode((int)HttpStatusCode.OK);
         }
-
     }
 }
