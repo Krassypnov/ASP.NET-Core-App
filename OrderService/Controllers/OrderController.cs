37e44f63b8e3543bf5449f245374f67c01ec9b5a
Krassypnov <krassypnov75@gmail.com>
2022-04-19 17:02:36 +0700

    Add controller for orders

---
+++ OrderService/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using OrderService.Models;
+using System.Net;
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public OrderController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        
+    }
+}
